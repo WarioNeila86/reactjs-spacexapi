96ec7e330898be7371c46b376a62ac7626178631
WarioNeila86 <mariogar1001@gmail.com>
2022-09-04 23:18:10 +0200

    retrieve and show all launches from SpaceX API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import * as API from './services/launches';
+import React, {useState, useEffect} from 'react';
+
 export function App() {
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    API.getAllLaunches().then(setLaunches).catch(console.error);
+  }, []);
+
 
   return (
-    <div>Hello world!</div>
+    <React.Fragment>
+      <h1>SpaceX Launches</h1>
+      <ul>
+        {launches.map(launch => (
+          <li key={`${launch.flight_number}_${launch.launch_date_unix}`}>
+            {launch.mission_name} {launch.launch_year}
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
   )
 }
