bf9bf5debee34202eb5bba38da5a4efd86daa6d3
WarioNeila86 <mariogar1001@gmail.com>
2022-09-05 23:06:14 +0200

    customize color for calendar icon and date

--- src/components/LaunchItem.jsx
+++ src/components/LaunchItem.jsx
@@ -1,4 +1,4 @@
-import { Box, Text, Spacer, Tag, Flex } from '@chakra-ui/react';
+import { Box, Text, Spacer, Tag, Flex, Icon } from '@chakra-ui/react';
 import { HiCalendar } from "react-icons/hi";
 import { format } from 'date-fns';
 
@@ -16,14 +16,14 @@ export function LaunchItem(launch) {
           Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
         </Text>
         <Spacer />
-        <Tag padding={4} colorScheme={launch.launch_success ? 'green' : 'red'}>
+        <Tag padding={2} colorScheme={launch.launch_success ? 'green' : 'red'}>
           {launch.launch_success ? 'Success' : 'Failure'}
         </Tag>
       </Flex>
 
       <Flex align="center">
-        <HiCalendar />
-        <Text marginLeft={1} fontSize="sm">
+        <Icon as={HiCalendar} color="gray.500" />
+        <Text marginLeft={1} fontSize="sm" color="gray.500">
           {format(new Date(launch.launch_date_local), 'd MMMM yyyy')}
         </Text>
       </Flex>
