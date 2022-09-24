bd70890605df7db9c56f1c377568e214e8642946
WarioNeila86 <6084473+WarioNeila86@users.noreply.github.com>
2022-09-25 01:34:59 +0200

    add youtube video to launch details page

--- src/components/LaunchDetails.jsx
+++ src/components/LaunchDetails.jsx
@@ -1,9 +1,9 @@
 import { useParams } from 'react-router-dom';
 import { React, useState, useEffect } from 'react';
 import {
-  Box, Button, Flex, Heading, Icon, Link, List, ListIcon, ListItem, Spacer, Spinner, Tag, Text
+  AspectRatio, Box, Button, Flex, Heading, Icon, Link, List, ListIcon, ListItem, Spacer, Spinner, Tag, Text
 } from '@chakra-ui/react';
-import { SiWikipedia, SiYoutube } from 'react-icons/si';
+import { SiWikipedia } from 'react-icons/si';
 import { BiArrowBack, BiNews } from 'react-icons/bi';
 import { IoAirplaneSharp, IoLocationSharp, IoRocketSharp } from 'react-icons/io5';
 import { format } from 'date-fns';
@@ -61,29 +61,15 @@ export function LaunchDetails() {
                   <List>
                     <ListItem>
                       <ListIcon as={IoAirplaneSharp} />
-                      <strong>Flight number:</strong>
-                      {' '}
-                      {launchDetails.flight_number}
+                      <strong>Flight number:</strong> {launchDetails.flight_number}
                     </ListItem>
                     <ListItem>
                       <ListIcon as={IoLocationSharp} />
-                      <strong>Launchpad:</strong>
-                      {' '}
-                      {launchpad.full_name}
+                      <strong>Launchpad:</strong> {launchpad.full_name}
                     </ListItem>
                     <ListItem>
                       <ListIcon as={IoRocketSharp} />
-                      <strong>Rocket:</strong>
-                      {' '}
-                      {rocket.name}
-                      {' '}
-                      -
-                      {' '}
-                      {rocket.company}
-                      {' '}
-                      (
-                      {rocket.country}
-                      )
+                      <strong>Rocket:</strong> {rocket.name} - {rocket.company} ({rocket.country})
                     </ListItem>
                   </List>
                   <CrewModal {...launchDetails} />
@@ -92,13 +78,18 @@ export function LaunchDetails() {
                     <Link href={launchDetails.links?.wikipedia} isExternal>
                       <Icon as={SiWikipedia} color="gray.500" />
                     </Link>
-                    <Link href={launchDetails.links?.webcast} isExternal>
-                      <Icon marginLeft={2} as={SiYoutube} color="gray.500" />
-                    </Link>
                     <Link href={launchDetails.links?.article} isExternal>
                       <Icon marginLeft={2} as={BiNews} color="gray.500" />
                     </Link>
                   </Box>
+                </Box>
+                <Box padding={7} maxWidth="700px" margin="auto">
+                  <AspectRatio>
+                    <iframe
+                      src={`https://www.youtube.com/embed/${launchDetails.links?.youtube_id}`}
+                      title="YouTube video"
+                    />
+                  </AspectRatio>
                 </Box>
                 <Link href="/" _hover={{ textDecoration: 'none' }}>
                   <Button size={['xs', 'xs', 'sm']} leftIcon={<BiArrowBack />} colorScheme="blackAlpha">
